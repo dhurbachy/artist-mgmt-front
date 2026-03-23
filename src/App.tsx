1dcd7ac4957bdb41ce81de74ee3e4cc4b2c3c42b
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-03-23 12:47:40 +0545

    feat: add dashboard blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 
 import './App.css'
-import { LoginForm } from "@/components/login-form"
-
+import Page from './app/dashboard/page'
 function App() {
 
   return (
     <>
-      <div className="dark flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
-        <div className="w-full max-w-sm md:max-w-4xl">
-          <LoginForm />
-        </div>
-      </div>
+      <div className='dark '>
+
+      <Page />      </div>
+
     </>
   )
 }
