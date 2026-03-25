5de7ba271cdfbb90346cab3602142ac0dcb61697
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-03-25 11:42:00 +0545

    feat:add fonts

--- src/app/auth/pages/login.tsx
+++ src/app/auth/pages/login.tsx
@@ -1,12 +1,11 @@
 import { LoginForm } from "@/components/login-form"
+
 export default function Login() {
     return (
-        <>
-            <div className="max-w-['400px']">
-
+        <div className="min-h-screen bg-[#feffe6] flex items-center justify-center px-4">
+            <div className="w-full max-w-sm">
                 <LoginForm />
             </div>
-
-        </>
+        </div>
     )
 }
