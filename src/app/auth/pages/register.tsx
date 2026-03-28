44fd2f588b7f52ddb5f68172e40413193eb14446
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-03-28 10:38:26 +0545

    update: validation check

--- src/app/auth/pages/register.tsx
+++ src/app/auth/pages/register.tsx
@@ -5,13 +5,13 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label"; // npx shadcn add label
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useRegister } from "../hooks/auth";
 import { ROUTES } from "@/routes/routeConstant";
 const registerSchema = z.object({
-  first_name: z.string().min(2, "Required"),
-  last_name: z.string().min(2, "Required"),
-  email: z.string().email("Invalid email"),
+  first_name: z.string().min(2, "First Name is Required"),
+  last_name: z.string().min(2, "Last Name is Required"),
+  email: z.string().email("Email is Required"),
   password: z.string().min(8, "Min 8 chars"),
   // Remove .default() and use .enum() directly to make it strictly required
   role: z.enum(["artist", "artist_manager"], "Please select a role"),
@@ -21,6 +21,7 @@ const registerSchema = z.object({
 type RegisterFormValues = z.infer<typeof registerSchema>;
 
 export default function Register() {
+  const navigate=useNavigate();
   const {mutate:registerMutate}=useRegister();
   
   const {
@@ -31,14 +32,21 @@ export default function Register() {
     formState: { errors },
   } = useForm<RegisterFormValues>({
     resolver: zodResolver(registerSchema),
-    defaultValues: { role: "artist" },
+    defaultValues: { 
+      role: "artist",
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+    },
+    mode: "onChange", 
   });
 
   const onSubmit = (values: RegisterFormValues) => {
-    console.log(values);
     registerMutate(values,{
       onSuccess:()=>{
-        // navigate(ROUTES.DASHBOARD);
+        // navigate(ROUTES.DASHBOARD)
+        navigate(ROUTES.LOGIN);
         reset();
 
       },
@@ -51,7 +59,7 @@ export default function Register() {
   return (
     <div className="min-h-screen bg-[#feffe6] flex items-center justify-center px-4">
       <div className="w-full max-w-sm">
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 max-w-xl mx-auto p-6 border rounded-xl bg-card">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 max-w-xl mx-auto p-6 border rounded-xl bg-card" noValidate>
           <div className="flex flex-col items-center gap-2 text-center">
             <h5 className="text-xl font-bold">Create New Account</h5>
             <p className="text-balance text-xs text-muted-foreground">
@@ -63,12 +71,12 @@ export default function Register() {
             <div className="space-y-2">
               <Label htmlFor="first_name">First Name</Label>
               <Input id="first_name" placeholder="John" {...register("first_name")} />
-              {errors.first_name && <p className="text-xs text-destructive">{errors.first_name.message}</p>}
+              {errors.first_name && <p className="text-xs text-destructive text-left">{errors.first_name.message}</p>}
             </div>
             <div className="space-y-2">
               <Label htmlFor="last_name">Last Name</Label>
               <Input id="last_name" placeholder="Doe" {...register("last_name")} />
-              {errors.last_name && <p className="text-xs text-destructive">{errors.last_name.message}</p>}
+              {errors.last_name && <p className="text-xs text-destructive text-left">{errors.last_name.message}</p>}
             </div>
           </div>
 
@@ -76,7 +84,7 @@ export default function Register() {
           <div className="space-y-2">
             <Label htmlFor="email">Email Address</Label>
             <Input id="email" type="email" placeholder="john@example.com" {...register("email")} />
-            {errors.email && <p className="text-xs text-destructive">{errors.email.message}</p>}
+            {errors.email && <p className="text-xs text-destructive text-left">{errors.email.message}</p>}
           </div>
 
           {/* Role & Gender (Custom Selects require Controller) */}
@@ -88,7 +96,7 @@ export default function Register() {
                 name="role"
                 render={({ field }) => (
                   <Select onValueChange={field.onChange} defaultValue={field.value}>
-                    <SelectTrigger><SelectValue placeholder="Role" /></SelectTrigger>
+                    <SelectTrigger className="w-full h-9 text-sm focus:border-emerald-500"><SelectValue placeholder="Role" /></SelectTrigger>
                     <SelectContent>
                       <SelectItem value="artist">Artist</SelectItem>
                       <SelectItem value="artist_manager">Manager</SelectItem>
@@ -105,7 +113,7 @@ export default function Register() {
                 name="gender"
                 render={({ field }) => (
                   <Select onValueChange={field.onChange} value={field.value}>
-                    <SelectTrigger><SelectValue placeholder="Select" /></SelectTrigger>
+                    <SelectTrigger className="w-full h-9 text-sm focus:border-emerald-500"><SelectValue placeholder="Select" /></SelectTrigger>
                     <SelectContent>
                       <SelectItem value="m">Male</SelectItem>
                       <SelectItem value="f">Female</SelectItem>
@@ -121,7 +129,7 @@ export default function Register() {
           <div className="space-y-2">
             <Label htmlFor="password">Password</Label>
             <Input id="password" type="password" {...register("password")} />
-            {errors.password && <p className="text-xs text-destructive">{errors.password.message}</p>}
+            {errors.password && <p className="text-xs text-destructive text-left">{errors.password.message}</p>}
           </div>
 
           <Button type="submit" className="w-full">Register</Button>
